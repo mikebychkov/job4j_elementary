e360f22981d3f60b03e1a398f63c8a324e79dcb7
MikeBychkov <MihailBichkov@yandex.ru>
2020-03-17 22:05:22 +0500

    3.3. Разбор первой программы.[#256979]

---
+++ src/ru/job4j/calculator/Calculator.java
@@ -0,0 +1,19 @@
+package ru.job4j.calculator;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int one = 1;
+        int two = 2;
+        int onePlusTwo = one + two;
+        System.out.println(onePlusTwo);
+        int six = 6;
+        int sixDivTwo = six / two;
+        System.out.println(sixDivTwo);
+        int five = 5;
+        int fiveMinusTwo = five - two;
+        System.out.println(fiveMinusTwo);
+        int four = 4;
+        int fourTimeTwo = four * two;
+        System.out.println(fourTimeTwo);
+    }
+}
