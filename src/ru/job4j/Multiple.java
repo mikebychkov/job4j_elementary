9df07fb2baffffcf3afc31a1b83522c497840c17
MikeBychkov <MihailBichkov@yandex.ru>
2020-03-17 23:42:01 +0500

    9.1. String[#256970]

--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -2,7 +2,7 @@ package ru.job4j;
 
 public class Multiple {
     public static void main(String[] args) {
-        for(int x=2; x<10; x++){
+        for (int x = 2; x < 10; x++) {
             System.out.println("1 * " + x + " = " + x);
         }
     }
