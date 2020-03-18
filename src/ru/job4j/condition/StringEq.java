2f4dda98b17be86e8ef39e2af5b9d380fe4533dc
MikeBychkov <MihailBichkov@yandex.ru>
2020-03-18 20:28:36 +0500

    4.1.0. Операторы сравнения в String.[#256926]

---
+++ src/ru/job4j/condition/StringEq.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class StringEq {
+
+    public static boolean check(String login) {
+        String root = new String("root");
+        return root.equals(login);
+    }
+
+    public static void main(String[] args) {
+        String your = "your_name";
+        boolean userHasAccess = StringEq.check(your);
+        System.out.println(userHasAccess);
+    }
+}
