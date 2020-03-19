96bcca9018988b893dcebe6d2d7de4f7247b75c7
MikeBychkov <MihailBichkov@yandex.ru>
2020-03-19 20:27:21 +0500

    6.4.1. Слово заканчивается с ...[#256940]

--- src/ru/job4j/array/FindLoop.java
+++ src/ru/job4j/array/FindLoop.java
@@ -11,5 +11,20 @@ public class FindLoop {
         }
         return rst;
     }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1; // если элемента нет в массиве, то возвращаем -1.
+        start = Math.max(start, 0);
+        finish = Math.max(finish, 0);
+        start = Math.min(start, data.length - 1);
+        finish = Math.min(finish, data.length - 1);
+        for (int x = start; x <= finish; x++) {
+            if (data[x] == el) {
+                rst = x;
+                break;
+            }
+        }
+        return rst;
+    }
 }
 
