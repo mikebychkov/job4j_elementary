c280b2c4a0967669918d520a5f8b6e1fd71634f2
MikeBychkov <MihailBichkov@yandex.ru>
2020-04-13 17:50:29 +0500

    6. Тестовое задание из модуля коллекции Lite переделать на Stream API.[#256902]

--- src/ru/job4j/bank/BankService.java
+++ src/ru/job4j/bank/BankService.java
@@ -1,9 +1,8 @@
 package ru.job4j.bank;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class BankService {
     private Map<User, List<Account>> users = new HashMap<>();
@@ -22,18 +21,30 @@ public class BankService {
     }
 
     public User findByPassport(String passport) {
+        /*
         for (User usr : this.users.keySet()) {
             if (usr.getPassport().equals(passport)) return usr;
         }
+        */
+        List<User> rsl = this.users.keySet().stream().filter(
+                usr -> usr.getPassport().equals(passport)
+        ).collect(Collectors.toList());
+        for (User usr : rsl) return usr;
         return null;
     }
 
     public Account findByRequisite(String passport, String requisite) {
         User usr = findByPassport(passport);
         if (usr == null) return null;
+        /*
         for (Account acc : this.users.get(usr)) {
             if (acc.getRequisite().equals(requisite)) return acc;
         }
+        */
+        List<Account> rsl = this.users.get(usr).stream().filter(
+                acc -> acc.getRequisite().equals(requisite)
+                ).collect(Collectors.toList());
+        for (Account acc : rsl) return acc;
         return null;
     }
 
