2d5b8d63c2cd3afa3beaa2fa737ddcbd95fdafb2
MikeBychkov <MihailBichkov@yandex.ru>
2020-04-14 19:07:33 +0500

    Отсортировать департаменты[#257024] #202004141907

--- src/ru/job4j/collection/DepDescComp.java
+++ src/ru/job4j/collection/DepDescComp.java
@@ -7,15 +7,8 @@ public class DepDescComp implements Comparator<String> {
     public int compare(String o1, String o2) {
         String[] as1 = o1.split("/");
         String[] as2 = o2.split("/");
-        int len = Math.min(as1.length, as2.length);
-        int rsl = 0;
-        rsl = as2[0].compareTo(as1[0]);
+        int rsl = as2[0].compareTo(as1[0]);
         if (rsl != 0) return rsl;
-        for (int i = 1; i < len; i++) {
-            rsl = as1[i].compareTo(as2[i]);
-            if (rsl != 0) return rsl;
-        }
-        rsl = as1.length - as2.length;
-        return rsl;
+        return o1.compareTo(o2);
     }
 }
